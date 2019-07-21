950f1441732a382c1e68b450cb7d6522010ebc19
Glussel <rob.w.gleason@gmail.com>
2019-07-21 15:05:34 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import Buttons from "./Button"
 
 function App() {
   return (
-    <div className="App">
-    <div className='Art'>
-    <h1>Hello World</h1> 
-    <h1><Button variant="contained" color="primary" >Hello!</Button></h1> 
-    <h1><Button variant="contained" color="primary" >First</Button></h1> 
-    <h1><Button variant="contained" color="primary" >Second</Button></h1> 
-    <h1><Button variant="contained" color="primary" >Third</Button></h1> 
-
- <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
-
-</div>
-    </div>
+    <Buttons/>
   );
 }
 
