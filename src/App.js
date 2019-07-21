04f0ab2f26422966428f87b87eeea68fe6e77ab2
Glussel <rob.w.gleason@gmail.com>
2019-07-21 14:27:41 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Art'>
+    <h1>Hello World</h1> 
+    <h1><Button variant="contained" color="primary" >Hello!</Button></h1> 
+    <h1><Button variant="contained" color="primary" >First</Button></h1> 
+    <h1><Button variant="contained" color="primary" >Second</Button></h1> 
+    <h1><Button variant="contained" color="primary" >Third</Button></h1> 
+
+ <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+
+</div>
     </div>
   );
 }
