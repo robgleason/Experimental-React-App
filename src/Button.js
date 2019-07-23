1076a24523c0ae87202c709f7181aa41f0272889
Glussel <rob.w.gleason@gmail.com>
2019-07-22 20:00:00 -0500

    Third commit

--- src/Button.js
+++ src/Button.js
@@ -3,20 +3,38 @@ import Button from '@material-ui/core/Button';
 
 
 class Buttons extends Component {
+   
     constructor(props) {
         super(props);
+        this.msg = "Hello"
+        
+         
       }
+       
+  
+      
+      handleClick() {
+        console.log('this is:', this);
+        
+      }
+
+
       render() {
+   
         return (
+            
             <div className="App">
             <div className='Art'>
             <h1>Hello World</h1> 
             <h1><Button variant="contained" color="primary" >Hello!</Button></h1> 
             <h1><Button variant="contained" color="primary" >First</Button></h1> 
             <h1><Button variant="contained" color="primary" >Second</Button></h1> 
-            <h1><Button variant="contained" color="primary" >Third</Button></h1> 
-        
-         <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+            
+             <h2>{this.msg}</h2>
+             <Button variant="contained" color="primary" onClick={(e) => this.handleClick(e)}>
+                 Click me for Console.log </Button>
+         
+         <h2>  {this.msg} Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
         
         </div>
             </div>
