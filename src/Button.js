56af4f0756286e650d61c132a43896b1a31a57f8
robgleason <46877134+robgleason@users.noreply.github.com>
2019-07-23 23:37:00 -0500

    Add files via upload

--- src/Button.js
+++ src/Button.js
@@ -7,12 +7,15 @@ class Buttons extends Component {
     constructor(props) {
         super(props);
         // this.msg = "Hello"
-        this.state = {  title: "Click here", msg: "Hello!" }        
+        this.state = {  title: "Click here", title2: "(No, here!)", msg: "Hello!" }        
       }
        
    
       changeTitle = () => {
         this.setState({ title: "New title" });
+     };
+      changeTitleTwo = () => {
+        this.setState({ title2: "Newer title" });
      };
 
       reset = () => {
@@ -40,7 +43,10 @@ class Buttons extends Component {
                  Check your console.log  </Button>
          
          <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
-         <h1 onClick={this.changeTitle}>{this.state.title}</h1>;
+         <h1 onClick={this.changeTitle}>{this.state.title}</h1>
+         <h1 onClick={this.changeTitleTwo}>{this.state.title2}</h1>
+         
+    
         </div>
             </div>
         );
