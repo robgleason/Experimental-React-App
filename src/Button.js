d37687a3bc33a088f87a55f3af1d75476d0e8d0e
Glussel <rob.w.gleason@gmail.com>
2019-07-25 22:03:32 -0500

    Pokemon function added

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
-
+import img0 from "./0.png";
+import img1 from "./1.png";
+import img2 from "./2.png";
+import img3 from "./3.png";
+import img4 from "./4.png";
+import img5 from "./5.png";
+import img6 from "./6.png";
 
 class Buttons extends Component {
    
     constructor(props) {
         super(props);
         // this.msg = "Hello"
-        this.state = {  title: "Click here", title2: "(No, here!)", msg: "Hello!" }        
+        this.state = {  title: "Click here", title2: "(No, here!)", msg: "Hello!", randNum: "0" } 
+       
+        
+        }; 
+        
+        static defaultProps = {
+          
+          images: [img0, img1, img2, img3, img4, img5, img6]
+
       }
        
    
@@ -21,6 +35,15 @@ class Buttons extends Component {
       reset = () => {
         this.setState({ msg: "Goodbye!" });
      };
+
+      rando = () => {
+       
+        
+        let x = toString(Math.random(0,6))
+        this.setState({ randNum: "2" });
+       
+       
+     };
       
       handleClick() {
         console.log('this is:', this);
@@ -29,7 +52,7 @@ class Buttons extends Component {
 
 
       render() {
-    
+      let randNum = 0;
         return (
             
             <div className="App">
@@ -45,7 +68,10 @@ class Buttons extends Component {
          <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
          <h1 onClick={this.changeTitle}>{this.state.title}</h1>
          <h1 onClick={this.changeTitleTwo}>{this.state.title2}</h1>
-         
+
+        <h1><Button variant="contained" color="primary" onClick={(e) => this.rando(e)}>Random Pokemon!</Button></h1> 
+
+         <img src={this.props.images[this.state.randNum]} alt="Random Picture"/>
     
         </div>
             </div>
